918045bc664f4bf08b6de1f4b5ae2b87a99b6d66
Pedro <pcunhavieira@gmail.com>
2020-06-25 20:11:18 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoMVCB32/UI/frmCliente.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using ProjetoMVCB32.BLL;
+using ProjetoMVCB32.DTO;
+
+namespace ProjetoMVCB32.UI
+{
+    public partial class frmCliente : System.Web.UI.Page
+    {
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            Pessoa obj = new Pessoa();
+
+            obj.Nome = txt_nomeB32.Text;
+            obj.Cpf = txt_cpfB32.Text;
+            obj.NomeMae = txt_nomemaeB32.Text;
+
+            tblClienteBLL bllCliente = new tblClienteBLL();
+            if (bllCliente.Autenticar(obj.Nome, obj.Cpf, obj.NomeMae))
+            {
+                txtMsg.Visible = true;
+                txtMsg.Text = "Beneficiário Localizado no Banco de dados. Processo em Análise";
+            }
+            else
+            {
+                txtMsg.Visible = true;
+                txtMsg.Text = "Beneficiário Não Localizado no Banco de dados. Benefício Negado";
+            }
+        }
+    }
+}
---
+++ ProjetoMVCB32/UI/frmCliente.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ProjetoMVCB32.UI
+{
+
+
+    public partial class frmCliente
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle txt_cpfB32.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_cpfB32;
+
+        /// <summary>
+        /// Controle txt_nomeB32.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nomeB32;
+
+        /// <summary>
+        /// Controle txt_nomemaeB32.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nomemaeB32;
+
+        /// <summary>
+        /// Controle btn_enviarB32.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_enviarB32;
+
+        /// <summary>
+        /// Controle txtMsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txtMsg;
+    }
+}
